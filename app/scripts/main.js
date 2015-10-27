885792fad25b3f41204c4c5c7306a94f6752bd37
Ollie Smith <ollie.smith@me.com>
2015-10-27 23:11:47 +0000

    intro area polish

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,57 +1,78 @@
 // Variable for static dots
-	var $dots=$(".menu-item")
-  // movable glob
-		,$current=$(".active")
+var $dots=$(".menu-item")
+// movable glob
+,$current=$(".active")
 
 
-    // Variable to measue spacing between dots
-		,spacing=53
-    // Halved spacing
-		,halfSpacing=spacing/2
-    // Empty global start position variable
-		,startPos
-	;
+// Variable to measue spacing between dots
+,spacing=53
+// Halved spacing
+,halfSpacing=spacing/2
+// Empty global start position variable
+,startPos
+;
 
-  // Set startPos equal to top position of first dot
-	startPos=3;
+// Set startPos equal to top position of first dot
+startPos=3;
+
+function alignIntro() {
+		var menuOffset = parseInt($('.menu').offset().top);
+		introOffset = parseInt($('.introduction').offset().top);
+		headerHeight = parseInt($('.header').offset().top);
+		introParaOffset = introOffset - headerHeight;
+		offsetRecalc = menuOffset - introParaOffset;
+		$('#section-one').css('padding-top', offsetRecalc);
+}
 
 function updatePos(){
-		var pos=$current.data("pos").y-startPos;
-		var scale=pos%spacing;
-	    if(scale>halfSpacing){
-	      scale=halfSpacing-(scale-halfSpacing);
-	    }
-	    scale=1-((scale/halfSpacing)*0.35);
-	    TweenMax.set($current,{
-	    	y:pos+startPos,
-	    	scale:scale*0.6,
-	    	force3D:true
-	    });
+	var pos=$current.data("pos").y-startPos;
+	var scale=pos%spacing;
+	if(scale>halfSpacing){
+		scale=halfSpacing-(scale-halfSpacing);
+	}
+	scale=1-((scale/halfSpacing)*0.35);
+	TweenMax.set($current,{
+		y:pos+startPos,
+		scale:scale*0.6,
+		force3D:true
+	});
 
-	    var curItem=pos/spacing,
-	    	curItemR=Math.round(curItem)
-	    ;
-    }
+	var curItem=pos/spacing,
+	curItemR=Math.round(curItem);
+}
 
 $(document).ready(function(){
 
-	  // Reset active glob to startPos
+	if ($(document).scrollTop() <= 80) {
+			alignIntro();
+	}
+
+	$(window).resize(function() {
+      if(this.resizeTO) clearTimeout(this.resizeTO);
+      this.resizeTO = setTimeout(function() {
+          $(this).trigger('afterResize');
+      }, 200);
+  });
+
+	$(window).bind('afterResize', function() {
+    alignIntro();
+	});
+
+	// Reset active glob to startPos
 	$current.data("pos",{y:startPos});
 
-  //When dot clicked
+	//When dot clicked
 	$dots.click(function(event){
 		var $cur=$(this);
-		// $('.menu-item').removeClass('visible');
-		// $cur.toggleClass('visible');
 
-    // Amount to move active glob by
+		// Amount to move active glob by
 		var dest=($cur.index())*spacing;
 
 		console.log(startPos);
 		console.log(spacing);
 		console.log(dest);
 
-    // Move active glob
+		// Move active glob
 		TweenMax.to($current.data("pos"),0.6,{
 			y:startPos+dest,
 			onUpdate:updatePos,
@@ -62,6 +83,6 @@ $(document).ready(function(){
 		});
 	});
 
-  // Simulate click on first dot to position
+	// Simulate click on first dot to position
 	$dots.eq(0).click();
 })
