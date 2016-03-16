b7e5d84c83fa928fb173d0a76ee5811ff5b35592
Ollie Smith <ollie.smith@me.com>
2016-03-16 22:18:45 +0000

    Adds portfolio view refinements, real project data

--- app/scripts/portfolio.js
+++ app/scripts/portfolio.js
@@ -13,7 +13,18 @@ import {Router, Route, Link} from 'react-router';
 // classNames module - classSet deprecated
 import classNames from'classnames';
 
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import {state} from 'scripts/app/state';
+
+const uniqueId = (function() {
+  var count = 0;
+  return function() {
+    return 'id' + count++;
+  }
+})();
+
+var pagerTriangle = [];
 
 const actions = {
   toggleNext: function(instance) {
@@ -35,11 +46,11 @@ const actions = {
     }
     state['current'+ instanceName] = prev;
     render(state);
-    console.log(state['current'+ instanceName]);
+    // console.log(state['current'+ instanceName]);
   },
   toggleSlide: function(id, instance) {
-    var instanceName = instance.toString();
-    var index = state[instanceName].map(function (el) {
+    var instanceName = instance.toString(),
+    index = state[instanceName].map(function (el) {
       return (
         el.id
       );
@@ -51,11 +62,29 @@ const actions = {
       state[instanceName + 'open'] = true;
     }
     render(state);
+
+    pagerTriangle = Snap.selectAll('#' + instance + '-app .pager-item path');
+
+    if (state[instanceName + 'open'] == false) {
+        animateToTriangle();
+    }
+    else {
+        animateToRectangle();
+    }
   },
   closeProjects: function(instance) {
     var instanceName = instance.toString();
     state[instanceName + 'open'] = false;
     render(state);
+
+    pagerTriangle = Snap.selectAll('#' + instance + '-app .pager-item path');
+
+    if (state[instanceName + 'open'] == false) {
+        animateToTriangle();
+    }
+    else {
+        animateToRectangle();
+    }
   }
 }
 
@@ -99,35 +128,35 @@ function getData(dataset) {
 }
 
 getData('projects').then(function(response) {
-  console.log(response);
-  render(state);
+  // console.log(response);
+  getData('art').then(function(response) {
+    // console.log(response);
+    render(state);
+  }, function(error) {
+    // console.log('no dice');
+  });
 }, function(error) {
   console.log('no dice');
 });
-getData('art').then(function(response) {
-  console.log(response);
-  render(state);
-}, function(error) {
-  console.log('no dice');
-});
-// setTimeout(function(){
-//     getData('art');
-// }, 500);
 
 
 class Portfolio extends React.Component {
   render() {
-    var instanceName = this.props.instance;
+    var instanceName = this.props.instance,
+    classes = classNames({
+      "portfolio" : true,
+      "portfolio-active" : state[instanceName + 'open']
+    });
     var portfolio;
     if (state[instanceName + 'open']) {
-      portfolio = <div><Close data={this.props.data} instance={this.props.instance}/><Projects data={this.props.data} instance={this.props.instance}/><Controls data={this.props.data} instance={this.props.instance} /><Pagination data={this.props.data} instance={this.props.instance}/></div>;
-    }
-    else {
-      portfolio = <div><Pagination data={this.props.data} instance={this.props.instance} /></div>;
+      portfolio = <div className="detail"><Close data={this.props.data} instance={this.props.instance}/><Projects data={this.props.data} instance={this.props.instance}/><Controls data={this.props.data} instance={this.props.instance} /></div>;
     }
     return (
-      <div className="portfolio">
+      <div className={classes}>
+      <ReactCSSTransitionGroup transitionName="detail" transitionEnterTimeout={500} transitionLeaveTimeout={900}>
       {portfolio}
+      </ReactCSSTransitionGroup>
+      <Pagination data={this.props.data} instance={this.props.instance} />
       </div>
     );
   }
@@ -135,6 +164,8 @@ class Portfolio extends React.Component {
     var instanceName = this.props.instance;
     state['current' + instanceName] = 0;
     state[instanceName + 'open'] = false;
+    pagerTriangle = Snap.selectAll('.portfolio:not(.portfolio-active) .pager-item path');
+    animateToTriangle();
   }
 }
 
@@ -144,7 +175,7 @@ class Close extends React.Component {
   }
   render() {
     return (
-      <button onClick={this.closeProjects.bind(this)}>X</button>
+      <button className="close-button" onClick={this.closeProjects.bind(this)}>X</button>
     );
   }
 }
@@ -155,7 +186,7 @@ class Projects extends React.Component {
     var slidesNodes = this.props.data.map(function (slideNode, index) {
       var isActive = state['current' + instanceName] === index;
       return (
-        <Project active={isActive} key={slideNode.id} imagePath={slideNode.imagePath} imageAlt={slideNode.imageAlt} title={slideNode.title} subtitle={slideNode.subtitle} text={slideNode.text} action={slideNode.action} actionHref={slideNode.actionHref} />
+        <Project active={isActive} key={slideNode.id} imagePathFull={slideNode.imagePathFull} imageAlt={slideNode.imageAlt} title={slideNode.title} subtitle={slideNode.subtitle} text={slideNode.text} action={slideNode.action} actionHref={slideNode.actionHref} />
       );
     });
     return (
@@ -174,13 +205,22 @@ class Project extends React.Component {
     });
     return (
       <div className={classes}>
-      <img src={this.props.imagePath} alt={this.props.imageAlt} />
-      <h2>{this.props.title}</h2>
-      <h3>{this.props.subtitle}</h3>
-      <p>{this.props.text}</p>
-      <a href={this.props.actionHref}>{this.props.action}</a>
+        <div className="project-image">
+          <img src={this.props.imagePathFull} alt={this.props.imageAlt} />
+        </div>
+        <div className="project-info">
+          <h2>{this.props.title}</h2>
+          <h3>{this.props.subtitle}</h3>
+          <p dangerouslySetInnerHTML={{__html: this.props.text}}></p>
+        </div>
       </div>
     );
+  }
+  componentDidMount() {
+    $('.project-info').perfectScrollbar({suppressScrollX: true});
+  }
+  componentDidUpdate() {
+    $('.project-info').perfectScrollbar({suppressScrollX: true});
   }
 }
 
@@ -202,17 +242,46 @@ class Controls extends React.Component {
 }
 
 class Pagination extends React.Component {
+  componentDidUpdate() {
+    pagerTriangle = Snap.selectAll('.portfolio:not(.portfolio-active) .pager-item path');
+    animateToTriangle();
+  }
   render() {
-    var instanceProp = this.props.instance;
+    var instanceProp = this.props.instance,
+    activeNode = state['current' + instanceProp];
     var paginationNodes = this.props.data.map(function(paginationNode, index) {
+      var isActive = activeNode === index;
       return (
-        <Pager id={paginationNode.id} key={paginationNode.id} title={paginationNode.title} instance={instanceProp} />
+        <Pager id={paginationNode.id} active={isActive} key={paginationNode.id} title={paginationNode.title} imagePath={paginationNode.imagePath} instance={instanceProp} />
       );
     });
+    if (state[instanceProp + 'open'] && paginationNodes.length > 6) {
+      if (activeNode > (paginationNodes.length -3)) {
+        console.log(paginationNodes.length);
+      }
+      (activeNode < 3) ? paginationNodes = paginationNodes.slice(0, 5) : ((activeNode > (paginationNodes.length -3)) ? paginationNodes = paginationNodes.slice((paginationNodes.length - 5), paginationNodes.length) : paginationNodes = paginationNodes.slice((activeNode -2), (activeNode + 3)));
+    }
     return (
       <div className="pagination">
       {paginationNodes}
       </div>
+    );
+  }
+}
+
+class Clip extends React.Component {
+  render() {
+    var id = uniqueId(),
+    fallbackShape = state[this.props.instance + 'open'] ? "M0,9L11 9 11 1.7 0 1.7z" : "M5.5,0.7L2.7 5.5 0 10.3 11 10.3z";
+    return (
+      <svg viewBox="0 0 11 11" className="pager-item">
+        <defs>
+          <pattern id={id} x="10" y="10" width="1" height="1" patternUnits="objectBoundingBox">
+            <image xlinkHref={this.props.imagePath} x="0" y="-3.5" width="12" height="17" />
+          </pattern>
+        </defs>
+        <path fill={"url(#" + id + ")"} d={fallbackShape} />
+      </svg>
     );
   }
 }
@@ -222,8 +291,14 @@ class Pager extends React.Component {
     actions.toggleSlide(this.props.id, this.props.instance);
   }
   render() {
+    var classes = classNames({
+      "pager" : true,
+      "pager-active" : this.props.active
+    });
     return (
-      <span className="pager" onClick={this.toggleSlide.bind(this)}>{this.props.title}</span>
+      <span className={classes} onClick={this.toggleSlide.bind(this)}>
+        <Clip imagePath={this.props.imagePath} instance={this.props.instance} />
+      </span>
     );
   }
 }
@@ -241,10 +316,24 @@ class Blog extends React.Component {
 function render(state) {
   ReactDOM.render(
     <Portfolio data={state.projects} instance="projects"/>,
-    document.getElementById('portfolio-app')
+    document.getElementById('projects-app')
   );
   ReactDOM.render(
     <Portfolio data={state.art} instance="art"/>,
     document.getElementById('art-app')
   );
+}
+
+function animateToRectangle() {
+  let squarePoints = "M0,9L11 9 11 1.7 0 1.7z";
+  pagerTriangle.animate({ d: squarePoints }, 1000, mina.easeinout);
+}
+function animateToTriangle() {
+  var triangles = ["M9.8,1.9L11,9.1L0.1,6.4L9.8,1.9z", "M11,8.1L1.7,11L0,0L11,8.1z", "M11,1.9L6.6,8.8L0,4L11,1.9z", "M11.1,10H0L5.8,1L11.1,10z", "M11,4.5l-5.2,6.3L0.1,1.2L11,4.5z", "M10.8,0.9L9.8,9.3L0.1,0.9H10.8z"];
+    var e = 0;
+    for (let i = 0; i < pagerTriangle.length; i++) {
+      (i > 5) ? ((i === 6) ? e = 0 : ((i % 5 === 0) ? e = 0 : null)) : null;
+      pagerTriangle[i].animate({ d: triangles[e] }, 1000, mina.easeinout);
+      e++;
+    }
 }
--- config.js
+++ config.js
@@ -20,7 +20,8 @@ System.config({
     "classnames": "npm:classnames@2.2.3",
     "core-js": "npm:core-js@1.2.6",
     "fetch": "npm:whatwg-fetch@0.10.1",
-    "react": "npm:react@0.14.6",
+    "react": "npm:react@0.14.7",
+    "react-addons-css-transition-group": "npm:react-addons-css-transition-group@0.14.7",
     "react-dom": "npm:react-dom@0.14.5",
     "react-router": "npm:react-router@2.0.0-rc5",
     "react-slick": "npm:react-slick@0.9.3",
@@ -30,9 +31,6 @@ System.config({
     },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.6.0"
-    },
-    "github:jspm/nodelibs-domain@0.1.0": {
-      "domain-browser": "npm:domain-browser@1.1.7"
     },
     "github:jspm/nodelibs-events@0.1.1": {
       "events": "npm:events@1.0.2"
@@ -48,16 +46,6 @@ System.config({
     },
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
-    },
-    "npm:amdefine@1.0.0": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "module": "github:jspm/nodelibs-module@0.1.0",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:asap@2.0.3": {
-      "domain": "github:jspm/nodelibs-domain@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
@@ -82,25 +70,8 @@ System.config({
     "npm:core-util-is@1.0.2": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
     },
-    "npm:domain-browser@1.1.7": {
-      "events": "github:jspm/nodelibs-events@0.1.1"
-    },
-    "npm:envify@3.4.0": {
-      "jstransform": "npm:jstransform@10.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "through": "npm:through@2.3.8"
-    },
-    "npm:esprima-fb@13001.1001.0-dev-harmony-fb": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
     "npm:fbjs@0.6.1": {
-      "core-js": "npm:core-js@1.2.6",
-      "loose-envify": "npm:loose-envify@1.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "promise": "npm:promise@7.1.1",
-      "ua-parser-js": "npm:ua-parser-js@0.7.10",
-      "whatwg-fetch": "npm:whatwg-fetch@0.9.0"
+      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:history@2.0.0-rc2": {
       "child_process": "github:jspm/nodelibs-child_process@0.1.0",
@@ -121,14 +92,6 @@ System.config({
     "npm:json2mq@0.2.0": {
       "string-convert": "npm:string-convert@0.2.1"
     },
-    "npm:jstransform@10.1.0": {
-      "base62": "npm:base62@0.1.1",
-      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
-      "esprima-fb": "npm:esprima-fb@13001.1001.0-dev-harmony-fb",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "source-map": "npm:source-map@0.1.31"
-    },
     "npm:loose-envify@1.1.0": {
       "js-tokens": "npm:js-tokens@1.0.2",
       "process": "github:jspm/nodelibs-process@0.1.2",
@@ -141,15 +104,14 @@ System.config({
     "npm:process@0.11.2": {
       "assert": "github:jspm/nodelibs-assert@0.1.0"
     },
-    "npm:promise@7.1.1": {
-      "asap": "npm:asap@2.0.3",
-      "fs": "github:jspm/nodelibs-fs@0.1.2"
-    },
     "npm:query-string@3.0.0": {
       "strict-uri-encode": "npm:strict-uri-encode@1.1.0"
     },
+    "npm:react-addons-css-transition-group@0.14.7": {
+      "react": "npm:react@0.14.7"
+    },
     "npm:react-dom@0.14.5": {
-      "react": "npm:react@0.14.6"
+      "react": "npm:react@0.14.7"
     },
     "npm:react-responsive-mixin@0.3.5": {
       "can-use-dom": "npm:can-use-dom@0.1.0",
@@ -170,12 +132,11 @@ System.config({
       "classnames": "npm:classnames@2.2.3",
       "json2mq": "npm:json2mq@0.2.0",
       "object-assign": "npm:object-assign@2.1.1",
-      "react": "npm:react@0.14.6",
+      "react": "npm:react@0.14.7",
       "react-responsive-mixin": "npm:react-responsive-mixin@0.3.5",
       "slick-carousel": "npm:slick-carousel@1.5.9"
     },
-    "npm:react@0.14.6": {
-      "envify": "npm:envify@3.4.0",
+    "npm:react@0.14.7": {
       "fbjs": "npm:fbjs@0.6.1",
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
@@ -192,12 +153,6 @@ System.config({
     "npm:slick-carousel@1.5.9": {
       "jquery": "npm:jquery@2.2.0"
     },
-    "npm:source-map@0.1.31": {
-      "amdefine": "npm:amdefine@1.0.0",
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2"
-    },
     "npm:stream-browserify@1.0.0": {
       "events": "github:jspm/nodelibs-events@0.1.1",
       "inherits": "npm:inherits@2.0.1",
@@ -205,13 +160,6 @@ System.config({
     },
     "npm:string_decoder@0.10.31": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0"
-    },
-    "npm:through@2.3.8": {
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "stream": "github:jspm/nodelibs-stream@0.1.0"
-    },
-    "npm:ua-parser-js@0.7.10": {
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
